baa50d79bf0cdd204527708478ac62e08e2e18a3
Brian Hamrick <brian.c.hamrick@gmail.com>
2019-12-23 18:00:18 -0800

    Basic tree example

---
+++ examples/basic.rs
@@ -0,0 +1,79 @@
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+#![allow(dead_code)]
+use std::cmp::Ordering;
+
+use redblack_algorithm::*;
+
+#[derive(Debug)]
+struct Node {
+    key: i32,
+    left: (Option<Box<Node>>, Color),
+    right: (Option<Box<Node>>, Color),
+}
+
+impl PackContext<i32, Box<Node>> for () {
+    fn unpack(&mut self, node: Box<Node>) -> RedBlackData<i32, Box<Node>> {
+        RedBlackData {
+            data: node.key,
+            left: node.left,
+            right: node.right,
+        }
+    }
+
+    fn pack(&mut self, data: RedBlackData<i32, Box<Node>>) -> Box<Node> {
+        Box::new(Node {
+            key: data.data,
+            left: data.left,
+            right: data.right,
+        })
+    }
+}
+
+#[derive(Debug)]
+struct Tree {
+    root: Option<Box<Node>>,
+}
+
+impl Tree {
+    fn new() -> Tree {
+        Tree {
+            root: None,
+        }
+    }
+
+    fn insert(&mut self, x: i32) {
+        self.root = Some(insert(
+            self.root.take(),
+            &mut |node: &RedBlackData<i32, Box<Node>>, _| match x.cmp(&node.data) {
+                Ordering::Less => Some((Direction::Left, ())),
+                Ordering::Equal => None,
+                Ordering::Greater => Some((Direction::Right, ())),
+            },
+            x,
+            &mut (),
+        ));
+    }
+
+    fn remove(&mut self, x: i32) {
+        self.root = delete(
+            self.root.take(),
+            &mut |node: &RedBlackData<i32, Box<Node>>, _| match x.cmp(&node.data) {
+                Ordering::Less => Some((Direction::Left, ())),
+                Ordering::Equal => None,
+                Ordering::Greater => Some((Direction::Right, ())),
+            },
+            &mut (),
+        );
+    }
+}
+
+fn main() {
+    let mut tree = Tree::new();
+    for i in 1..10 {
+        tree.insert(i);
+    }
+    println!("{:?}", tree);
+    tree.remove(4);
+    println!("{:?}", tree);
+}
